6af1ee50600a28f6b2ab3c6e4a2114231e2d0633
jinyyash <jinyyash@gmail.com>
2019-09-15 21:15:05 +0530

    web view

---
+++ registrationIntegration-master/web-client/src/test/java/com/hsm/WebClientApplicationTest.java
@@ -0,0 +1,20 @@
+package com.hsm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple WebClientApplication.
+ */
+public class WebClientApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
